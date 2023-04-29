b5b60f245671ba093d2508fbcdf0d3f6f87c8dea
Jonas Liepe <jonas.liepe@moejo.net>
2023-04-29 23:38:07 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@
 if [ -f /opt/karaf/firstboot ]
 then
     /opt/karaf/bin/initkaraf
-    lsof /opt/karaf/bin/initkaraf
-    rm /opt/karaf/bin/initkaraf
+    #rm /opt/karaf/bin/initkaraf
 fi
 
 /opt/karaf/bin/checkvoletc
